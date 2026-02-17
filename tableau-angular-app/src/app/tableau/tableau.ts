b296bca8c4d21ffc1fb4e99ace13b807a7bc99da
LovelyPS <206708537+LovelyPS-predigle@users.noreply.github.com>
2026-02-17 12:29:05 +0530

    filteration out event done

--- tableau-angular-app/src/app/tableau/tableau.ts
+++ tableau-angular-app/src/app/tableau/tableau.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { ChangeDetectorRef, Component, CUSTOM_ELEMENTS_SCHEMA, ElementRef, ViewChild } from '@angular/core';
 import { TableauService } from '../services/tableau.service';
 import { CommonModule } from '@angular/common';
 
@@ -14,6 +14,10 @@ export class Tableau {
   // https://prod-in-a.online.tableau.com/t/lovelysaimon-6528126ab1/views/product_cost/Dashboard1%27
  vizUrl = "https://prod-in-a.online.tableau.com/t/lovelysaimon-6528126ab1/views/product_cost/Dashboard1";
   token: string = '';
+
+  @ViewChild('vizRef') vizElement!: ElementRef;
+
+currentFilters: any[] = [];
 
   constructor(private tableauService: TableauService,private cdr: ChangeDetectorRef) {}
 
@@ -46,8 +50,48 @@ export class Tableau {
     activeSheet.applyFilterAsync("Brand Name", ["Contoso"], "replace");
   }
 
-  onFilterChanged(event: any) {
-    console.log("Filter changed:", event.detail);
+  async onFilterChanged(event: any) {
+
+  const viz = this.vizElement.nativeElement;
+  const workbook = viz.workbook;
+  const activeSheet = workbook.activeSheet;
+
+  // If dashboard, get filters from all worksheets
+  if (activeSheet.sheetType === 'dashboard') {
+
+    const worksheets = activeSheet.worksheets;
+    let allFilters: any[] = [];
+
+    for (const sheet of worksheets) {
+      const filters = await sheet.getFiltersAsync();
+      allFilters = [...allFilters, ...filters];
+    }
+
+    this.currentFilters = this.formatFilters(allFilters);
+
+  } else {
+    const filters = await activeSheet.getFiltersAsync();
+    this.currentFilters = this.formatFilters(filters);
   }
+
+  console.log("All Current Filters:", this.currentFilters);
+}
+
+  formatFilters(filters: any[]) {
+
+  return filters.map(f => {
+
+    let values: any[] = [];
+
+    if (f.appliedValues) {
+      values = f.appliedValues.map((v: any) => v.value);
+    }
+
+    return {
+      field: f.fieldName,
+      values: values
+    };
+  });
+}
 
 }
